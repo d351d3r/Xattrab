919c966b9738ed7d18e27aa0c972dd3da99327ed
Anatolii Cherednichenko <greatestcombinator@gmail.com>
2021-02-07 19:37:48 +1000

    Update WriteBinary.c

--- WriteBinary.c
+++ WriteBinary.c
@@ -2,7 +2,6 @@
 // xattr creates a test attribute.
 
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
 #include <sys/xattr.h>
 
@@ -12,13 +11,13 @@
 
 
 int main(int argc, char *argv[]) {
+    (void)argc;
     FILE *path = fopen(argv[1], "wb");
     if (path) {
-        srand((unsigned int) time(NULL));
         short num_bytes = 64;
         unsigned char *stream = malloc(num_bytes);
-        for (size_t i = 0; i < num_bytes; i++) {
-            stream[i] = rand();
+        for (short i = 0; i < num_bytes; i++) {
+            stream[i] = random();
             fwrite(&stream[i], 1, 1, path);
             setxattr( argv[1], XATTR_BINARY_KEY, XATTR_TEST_VALUE, XATTR_TEST_VALUE_SIZE, 0);
         }
